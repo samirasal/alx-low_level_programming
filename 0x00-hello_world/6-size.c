1170524937ff946bd4033e33e69adaa2dca8d282
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-20 08:49:57 -0800

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int b;
 long int c;
 long long int d;
 float e;
-printf("Size of char: %zu byte(s)\n" (unsigned long)sizeof(a));
-printf("Size of int: %zu byte(s)\n" (unsigned long)sizeof(b));
-printf("Size of long int: %zu byte(s)\n" (unsigned long)sizeof(c));
-printf("Size of long long int: %zu byte(s)\n"(unsigned long)sizeof(d));
+printf("Size of char: %lu byte(s)\n" (unsigned long)sizeof(a));
+printf("Size of int: %lu byte(s)\n" (unsigned long)sizeof(b));
+printf("Size of long int: %lu byte(s)\n" (unsigned long)sizeof(c));
+printf("Size of long long int: %lu byte(s)\n"(unsigned long)sizeof(d));
 printf("Size of float: %lu byte(s)\n"(unsigned long)sizeof(e));
 return (0);
 }
