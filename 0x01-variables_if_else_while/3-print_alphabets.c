ef3fad8ae95f751945cb8dbb66b7d9cfb250e216
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-24 14:08:35 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,15 +11,15 @@ int main(void)
 	char c = 'a';
 	char C = 'A';
 
-	while (c <= 'z')
-	{
 	while (C <= 'Z')
 	{
-	putchar(C);
-	C++;
-	}
+	while (c <= 'z')
+	{
 	putchar(c);
 	c++;
+	}
+	putchar(C);
+	C++;
 	}
 	putchar('\n');
 	return (0);
