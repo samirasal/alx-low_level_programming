3a9ae90c2649908c22e64a85682c9b5daaaf9849
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-24 09:47:08 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ * Return: Always Success
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	while (c = 'a' && c <= 'z', c++)
+	{
+	putchar(c);
+	}
+	return (0);
+}
