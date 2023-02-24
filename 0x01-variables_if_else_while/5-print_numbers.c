ca582fe2b51e88274f91d68ba0684f691862a89d
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-24 15:39:49 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single numbers starting from 0
+ * Return: Always Success
+ *
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num = 1; num <= 10; num++)
+	{
+	printf("%d\n", num);
+	}
+	return (0);
+}
+
