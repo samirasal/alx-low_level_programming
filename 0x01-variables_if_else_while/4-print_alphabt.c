6be7899f8c8a33cd2d87ceb1ddd7ec0d2f69fb99
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-24 14:55:21 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in lowercase except q and e
+ * Return: Always Success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	while (c != 'q' && c != 'e')
+	{
+	putchar(c);
+	c++;
+	}
+	}
+	putchar('\n');
+	return (0);
+}
