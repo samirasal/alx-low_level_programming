a27e32a8a8df4ddbad59d2cc2a7f7af575149a1e
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-24 22:52:17 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single digit numbers
+ * Return: Always Success
+ *
+ */
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+	putchar(num);
+	putchar(',');
+	putchar(' ');
+	num++;
+	}
+	return (0);
+}
