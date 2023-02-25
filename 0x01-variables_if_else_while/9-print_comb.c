aff3bd4e60057cef0cca4dda7a14d74e4f0acc3d
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-25 07:25:34 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,17 @@
  */
 int main(void)
 {
-	int num = '0';
+	int num;
 
-	if (num <= '9')
+	for (num = '0'; num <= '9'; num++)
 	{
 	putchar(num);
+	if (num != '9')
+	{
 	putchar(',');
 	putchar(' ');
-	num++;
 	}
-	else if ((num = '9'))
-	{
+	}
 	putchar('\n');
-	}
 	return (0);
 }
