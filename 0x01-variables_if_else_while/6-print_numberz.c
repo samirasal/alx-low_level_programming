34f7d22d72ee214226f777b8de97551a570928c9
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-24 17:48:16 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digits starting from 0
+ * Return: Always Success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+	putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
