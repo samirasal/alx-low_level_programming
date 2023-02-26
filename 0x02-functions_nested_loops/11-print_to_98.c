b98ec00146e3fb1b45a6e85941bab41df7e39200
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-25 23:46:09 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,17 @@ void print_to_98(int n)
 	{
 	for (; n <= 98; n++)
 	{
+	if (n == 98)
+	{
 	printf("%d", n);
 	printf("\n");
 	break;
 	}
-	}
-	else if
+	else
 	{
 	printf("%d, ", n);
+	}
+	}
 	}
 	else
 	{
