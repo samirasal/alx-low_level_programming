9d4aa95df5cbf4280779cacfb87db8521d42f9f0
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-27 06:15:16 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.
+ * Return: Always Success
+ *
+ */
+
+int main(void)
+{
+
