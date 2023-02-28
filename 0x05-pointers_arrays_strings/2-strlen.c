5f602e13fa0754e68dbf11a676bef44f4ca66f21
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-27 17:34:22 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the data type char of the parameter
+ *
+ * Return: Always Success
+ */
+
+int _strlen(char *s)
+{
+	char *s = 0;
+
+	while (*s != '\0')
+	{
+	_putchar(strlen(s));
+	s++
+	}
+	return (0);
+}
