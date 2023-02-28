9a013ffe56ab98e5b4b2f1e56591f9c0af24143d
Samira Salifu <samirasalifu5522@gmail.com>
2023-02-27 17:07:36 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,26 +11,3 @@ void reset_to_98(int *n);
 {
 	*n = 98;
 }
-
-/**
- * main - how to change the value of a variable from outside the function
- * It uses a pointer
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-	int m;
-	int *p;
-
-	p = &m;
-	m = 402;
-
-	_putchar(402);
-	reset_to_98(p);
-	_putchar(n);
-	_putchar('\n');
-	return (0);
-}
-
