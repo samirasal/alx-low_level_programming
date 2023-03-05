90340270421dffc43022d3ad5a91386785c7bfc1
Samira Salifu <samirasalifu5522@gmail.com>
2023-03-04 22:44:47 -0800

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - Prints if number is positive or negative
+ * @i: represents the parameter of the function
+ * Return: Always Success
+ *
+ */
+
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+	{
+	printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+	printf("%d is zero\n", i);
+	}
+	else
+	{
+	printf("%d is negative\n", i);
+	}
+}
