bf011775beb9592f9688b7a48895f5287715b59e
Samira Salifu <samirasalifu5522@gmail.com>
2023-03-07 01:03:50 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - the function that prints numbers 0 to 9
+ * Return: Success
+ */
+
+void print_numbers(void)
+{
+	if (c >= 48 && c <= 57)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
