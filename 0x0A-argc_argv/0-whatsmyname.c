adab6da8d03daa8810bfae3b18f967af7c244251
Samira Salifu <samirasalifu5522@gmail.com>
2023-03-10 19:29:31 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Success
+ */
+
+int main(int argc__attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
